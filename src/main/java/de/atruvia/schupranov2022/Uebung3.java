85b5e9a696fff66dc086bdcf0a062d915707385d
Tobias Fink <finktobias@outlook.de>
2022-10-19 14:57:20 +0200

    Initialer Commit

---
+++ src/main/java/de/atruvia/schupranov2022/Uebung3.java
@@ -0,0 +1,42 @@
+package de.atruvia.schupranov2022;
+
+public class Uebung3
+{
+    public static void main(String[] args)
+    {
+        /* --- Deklaration und Initialisierung der benötigten Variablen --- */
+        double monatlicheEinzahlung = 200;      // Höhe der monatlichen Einzahlungen in Euro
+        double zinssatz = 4.5;                  // Zinssatz in Prozent
+        int laufzeit = 72;                      // Mindestlaufzeit/Dauer der Sparphase in Monaten
+
+        double bausparguthaben = 0.0;           // Aktuelles Bausparguthaben (angespartes Vermögen) in Euro
+
+        /* --- Ausgabe der Variablen --- */
+        System.out.println("--- Berechnungsgrundlage ---");
+        System.out.println("Höhe der monatlichen Einzahlungen: " + monatlicheEinzahlung + " EUR");
+        System.out.println("Zinssatz: " + zinssatz + " %");
+        System.out.println("Mindestlaufzeit/Dauer der Sparphase: " + laufzeit + " Monate");
+
+        /* --- Überprüfen, ob die Mindestlaufzeit größer null ist --- */
+        if (laufzeit == 0)
+        {
+            System.out.println("Die Mindestlaufzeit/Dauer der Sparphase darf nicht null sein!");
+        }
+
+        System.out.println();
+
+        /* --- Durchführung der Berechnung und Ausgabe des monatlichen Bausparguthabens --- */
+        for (int aktuellerMonat = 1; aktuellerMonat <= laufzeit; aktuellerMonat++)
+        {
+            bausparguthaben = bausparguthaben + monatlicheEinzahlung;
+
+            if (aktuellerMonat % 12 == 0)
+            {
+                double zinsen = bausparguthaben * zinssatz / 100;
+                bausparguthaben = bausparguthaben + zinsen;
+            }
+
+            System.out.println("Monat: " + aktuellerMonat + " | Aktuelles Bausparguthaben: " + bausparguthaben);
+        }
+    }
+}
---
+++ src/main/java/de/atruvia/schupranov2022/Uebung4.java
@@ -0,0 +1,60 @@
+package de.atruvia.schupranov2022;
+
+import java.util.Scanner;
+
+public class Uebung4
+{
+    public static void main(String[] args)
+    {
+        /* --- Deklaration und Initialisierung der benötigten Variablen --- */
+        double monatlicheEinzahlung = 200;      // Höhe der monatlichen Einzahlungen in Euro
+        double zinssatz = 4.5;                  // Zinssatz in Prozent
+        int laufzeit = 72;                      // Mindestlaufzeit/Dauer der Sparphase in Monaten
+
+        double bausparguthaben = 0.0;           // Aktuelles Bausparguthaben (angespartes Vermögen) in Euro
+
+        /* --- Einlesen der Werte über die Konsole --- */
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("--- Eingabe der Werte ---");
+
+        System.out.println("Höhe der monatlichen Einzahlungen: ");
+        monatlicheEinzahlung = scanner.nextDouble();
+
+        System.out.println("Zinssatz in Prozent: ");
+        zinssatz = scanner.nextDouble();
+
+        System.out.println("Mindestlaufzeit/Dauer der Sparphase in Monaten: ");
+        laufzeit = scanner.nextInt();
+
+        System.out.println();
+
+        /* --- Ausgabe der Variablen --- */
+        System.out.println("--- Berechnungsgrundlage ---");
+        System.out.println("Höhe der monatlichen Einzahlungen: " + monatlicheEinzahlung + " EUR");
+        System.out.println("Zinssatz: " + zinssatz + " %");
+        System.out.println("Mindestlaufzeit/Dauer der Sparphase: " + laufzeit + " Monate");
+
+        /* --- Überprüfen, ob die Mindestlaufzeit größer null ist --- */
+        if (laufzeit == 0)
+        {
+            System.out.println("Die Mindestlaufzeit/Dauer der Sparphase darf nicht null sein!");
+        }
+
+        System.out.println();
+
+        /* --- Durchführung der Berechnung und Ausgabe des monatlichen Bausparguthabens --- */
+        for (int aktuellerMonat = 1; aktuellerMonat <= laufzeit; aktuellerMonat++)
+        {
+            bausparguthaben = bausparguthaben + monatlicheEinzahlung;
+
+            if (aktuellerMonat % 12 == 0)
+            {
+                double zinsen = bausparguthaben * zinssatz / 100;
+                bausparguthaben = bausparguthaben + zinsen;
+            }
+
+            System.out.println("Monat: " + aktuellerMonat + " | Aktuelles Bausparguthaben: " + bausparguthaben);
+        }
+    }
+}
